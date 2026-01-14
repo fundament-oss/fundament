7a38795f736050a2b8cae5f72ea94bb514425e11
Chiel Timmermans <chiel.timmermans@aceworks.nl>
2026-01-14 09:16:34 +0100

    feat: projects (#33)

---
+++ db/migrations/005_projects.up.sql
@@ -0,0 +1,34 @@
+SET SESSION statement_timeout = 3000;
+SET SESSION lock_timeout = 3000;
+
+ALTER INDEX "tenant"."projects_uq_organization_name" RENAME TO "pgschemadiff_tmpidx_projects_uq_organiza_Ix$cGeM_QGSHMMM4V9Vpjw";
+
+ALTER TABLE "tenant"."projects" ADD COLUMN "deleted" timestamp with time zone;
+
+/* Hazards:
+ - AUTHZ_UPDATE: Adding a permissive policy could allow unauthorized access to data.
+*/
+CREATE POLICY "projects_organization_isolation" ON "tenant"."projects"
+	AS PERMISSIVE
+	FOR ALL
+	TO fun_fundament_api
+	USING ((organization_id = (current_setting('app.current_organization_id'::text))::uuid));
+
+/* Hazards:
+ - AUTHZ_UPDATE: Enabling RLS on a table could cause queries to fail if not correctly configured.
+*/
+ALTER TABLE "tenant"."projects" ENABLE ROW LEVEL SECURITY;
+
+/* Hazards:
+ - ACQUIRES_SHARE_LOCK: Non-concurrent index creates will lock out writes to the table during the duration of the index build.
+*/
+CREATE UNIQUE INDEX projects_uq_organization_name ON tenant.projects USING btree (organization_id, name, deleted) NULLS NOT DISTINCT;
+
+ALTER TABLE "tenant"."projects" ADD CONSTRAINT "projects_uq_organization_name" UNIQUE USING INDEX "projects_uq_organization_name";
+
+/* Hazards:
+ - ACQUIRES_ACCESS_EXCLUSIVE_LOCK: Index drops will lock out all accesses to the table. They should be fast.
+ - INDEX_DROPPED: Dropping this index means queries that use this index might perform worse because they will no longer will be able to leverage it.
+*/
+ALTER TABLE "tenant"."projects" DROP CONSTRAINT "pgschemadiff_tmpidx_projects_uq_organiza_Ix$cGeM_QGSHMMM4V9Vpjw";
+
