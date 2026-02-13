165218890d6a41851658f6d396b3b4896ca87579
Jos van der Maas <jos.vandermaas@vng.nl>
2026-02-13 09:18:38 +0100

    feat: protect all routes except login with auth

--- console-frontend/src/app/app.routes.ts
+++ console-frontend/src/app/app.routes.ts
@@ -8,195 +8,203 @@ const routes: Routes = [
     loadComponent: () => import('./login/login.component').then((m) => m.default),
   },
   {
-    path: 'add-cluster',
-    loadComponent: () =>
-      import('./add-cluster-wizard-layout/add-cluster-wizard-layout.component').then(
-        (m) => m.default,
-      ),
-    data: {
-      breadcrumbs: [
-        { label: 'Clusters', route: '/' },
-        { label: 'Add cluster', route: '/add-cluster' },
-      ],
-    },
+    path: '',
+    canActivate: [authGuard],
     children: [
       {
-        path: '',
-        loadComponent: () => import('./add-cluster/add-cluster.component').then((m) => m.default),
-        canActivate: [clusterWizardGuard],
+        path: 'add-cluster',
+        loadComponent: () =>
+          import('./add-cluster-wizard-layout/add-cluster-wizard-layout.component').then(
+            (m) => m.default,
+          ),
         data: {
-          breadcrumbs: [{ label: 'Basics' }],
+          breadcrumbs: [
+            { label: 'Clusters', route: '/' },
+            { label: 'Add cluster', route: '/add-cluster' },
+          ],
+        },
+        children: [
+          {
+            path: '',
+            loadComponent: () =>
+              import('./add-cluster/add-cluster.component').then((m) => m.default),
+            canActivate: [clusterWizardGuard],
+            data: {
+              breadcrumbs: [{ label: 'Basics' }],
+            },
+          },
+          {
+            path: 'nodes',
+            loadComponent: () =>
+              import('./add-cluster-nodes/add-cluster-nodes.component').then((m) => m.default),
+            canActivate: [clusterWizardGuard],
+            data: {
+              breadcrumbs: [{ label: 'Worker nodes' }],
+            },
+          },
+          {
+            path: 'plugins',
+            loadComponent: () =>
+              import('./add-cluster-plugins/add-cluster-plugins.component').then((m) => m.default),
+            canActivate: [clusterWizardGuard],
+            data: {
+              breadcrumbs: [{ label: 'Plugins' }],
+            },
+          },
+          {
+            path: 'summary',
+            loadComponent: () =>
+              import('./add-cluster-summary/add-cluster-summary.component').then((m) => m.default),
+            canActivate: [clusterWizardGuard],
+            data: {
+              breadcrumbs: [{ label: 'Summary' }],
+            },
+          },
+        ],
+      },
+      {
+        path: 'clusters/:id/nodes',
+        loadComponent: () =>
+          import('./cluster-nodes/cluster-nodes.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [
+            { label: 'Clusters', route: '/' },
+            { label: 'Cluster details' },
+            { label: 'Nodes' },
+          ],
         },
       },
       {
-        path: 'nodes',
+        path: 'clusters/:id/plugins',
         loadComponent: () =>
-          import('./add-cluster-nodes/add-cluster-nodes.component').then((m) => m.default),
-        canActivate: [clusterWizardGuard],
+          import('./cluster-plugins/cluster-plugins.component').then((m) => m.default),
         data: {
-          breadcrumbs: [{ label: 'Worker nodes' }],
+          breadcrumbs: [
+            { label: 'Clusters', route: '/' },
+            { label: 'Cluster details' },
+            { label: 'Plugins' },
+          ],
+        },
+      },
+      {
+        path: 'projects',
+        loadComponent: () => import('./projects/projects.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Projects', route: '/projects' }],
+        },
+      },
+      {
+        path: 'projects/add',
+        loadComponent: () => import('./add-project/add-project.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Projects', route: '/projects' }, { label: 'Add project' }],
+        },
+      },
+      {
+        path: 'projects/:id',
+        loadComponent: () =>
+          import('./project-detail/project-detail.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'General' }],
+        },
+      },
+      {
+        path: 'clusters/:id',
+        loadComponent: () =>
+          import('./cluster-details/cluster-details.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Clusters', route: '/' }, { label: 'Cluster details' }],
+        },
+      },
+      {
+        path: 'projects/:id/namespaces',
+        loadComponent: () => import('./namespaces/namespaces.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Namespaces' }],
+        },
+      },
+      {
+        path: 'projects/:id/members',
+        loadComponent: () =>
+          import('./project-members/project-members.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Members' }],
+        },
+      },
+      {
+        path: 'projects/:id/settings',
+        loadComponent: () =>
+          import('./project-settings/project-settings.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Settings' }],
         },
       },
       {
         path: 'plugins',
-        loadComponent: () =>
-          import('./add-cluster-plugins/add-cluster-plugins.component').then((m) => m.default),
-        canActivate: [clusterWizardGuard],
+        loadComponent: () => import('./plugins/plugins.component').then((m) => m.default),
         data: {
           breadcrumbs: [{ label: 'Plugins' }],
         },
       },
       {
-        path: 'summary',
-        loadComponent: () =>
-          import('./add-cluster-summary/add-cluster-summary.component').then((m) => m.default),
-        canActivate: [clusterWizardGuard],
+        path: 'profile',
+        loadComponent: () => import('./profile/profile.component').then((m) => m.default),
         data: {
-          breadcrumbs: [{ label: 'Summary' }],
+          breadcrumbs: [{ label: 'Profile' }],
+        },
+      },
+      {
+        path: 'plugins/:id',
+        loadComponent: () =>
+          import('./plugin-details/plugin-details.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Plugins', route: '/plugins' }, { label: 'Plugin details' }],
+        },
+      },
+      {
+        path: 'usage',
+        loadComponent: () => import('./usage/usage.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Usage' }],
+        },
+      },
+      {
+        path: 'projects/:id/usage',
+        loadComponent: () => import('./usage/usage.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Usage' }],
+        },
+      },
+      {
+        path: 'organization',
+        loadComponent: () => import('./organization/organization.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Organization' }],
+        },
+      },
+      {
+        path: 'organization/members',
+        loadComponent: () =>
+          import('./organization-members/organization-members.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Organization members' }],
+        },
+      },
+      {
+        path: 'api-keys',
+        loadComponent: () => import('./api-keys/api-keys.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'API keys' }],
+        },
+      },
+      {
+        path: '',
+        loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.default),
+        data: {
+          breadcrumbs: [{ label: 'Clusters' }],
         },
       },
     ],
-  },
-  {
-    path: 'clusters/:id/nodes',
-    loadComponent: () => import('./cluster-nodes/cluster-nodes.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [
-        { label: 'Clusters', route: '/' },
-        { label: 'Cluster details' },
-        { label: 'Nodes' },
-      ],
-    },
-  },
-  {
-    path: 'clusters/:id/plugins',
-    loadComponent: () =>
-      import('./cluster-plugins/cluster-plugins.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [
-        { label: 'Clusters', route: '/' },
-        { label: 'Cluster details' },
-        { label: 'Plugins' },
-      ],
-    },
-  },
-  {
-    path: 'projects',
-    loadComponent: () => import('./projects/projects.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Projects', route: '/projects' }],
-    },
-  },
-  {
-    path: 'projects/add',
-    loadComponent: () => import('./add-project/add-project.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Projects', route: '/projects' }, { label: 'Add project' }],
-    },
-  },
-  {
-    path: 'projects/:id',
-    loadComponent: () => import('./project-detail/project-detail.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'General' }],
-    },
-  },
-  {
-    path: 'clusters/:id',
-    loadComponent: () =>
-      import('./cluster-details/cluster-details.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Clusters', route: '/' }, { label: 'Cluster details' }],
-    },
-  },
-  {
-    path: 'projects/:id/namespaces',
-    loadComponent: () => import('./namespaces/namespaces.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Namespaces' }],
-    },
-  },
-  {
-    path: 'projects/:id/members',
-    loadComponent: () =>
-      import('./project-members/project-members.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Members' }],
-    },
-  },
-  {
-    path: 'projects/:id/settings',
-    loadComponent: () =>
-      import('./project-settings/project-settings.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Settings' }],
-    },
-  },
-  {
-    path: 'plugins',
-    loadComponent: () => import('./plugins/plugins.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Plugins' }],
-    },
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./profile/profile.component').then((m) => m.default),
-    canActivate: [authGuard],
-    data: {
-      breadcrumbs: [{ label: 'Profile' }],
-    },
-  },
-  {
-    path: 'plugins/:id',
-    loadComponent: () => import('./plugin-details/plugin-details.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Plugins', route: '/plugins' }, { label: 'Plugin details' }],
-    },
-  },
-  {
-    path: 'usage',
-    loadComponent: () => import('./usage/usage.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Usage' }],
-    },
-  },
-  {
-    path: 'projects/:id/usage',
-    loadComponent: () => import('./usage/usage.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: ':projectName', route: '/projects/:id' }, { label: 'Usage' }],
-    },
-  },
-  {
-    path: 'organization',
-    loadComponent: () => import('./organization/organization.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Organization' }],
-    },
-  },
-  {
-    path: 'organization/members',
-    loadComponent: () =>
-      import('./organization-members/organization-members.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'Organization members' }],
-    },
-  },
-  {
-    path: 'api-keys',
-    loadComponent: () => import('./api-keys/api-keys.component').then((m) => m.default),
-    data: {
-      breadcrumbs: [{ label: 'API keys' }],
-    },
-  },
-  {
-    path: '',
-    loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.default),
-    canActivate: [authGuard],
-    data: {
-      breadcrumbs: [{ label: 'Clusters' }],
-    },
   },
 ];
 
