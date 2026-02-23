0caa7a990a21ff35c6df7556fd4c9bbd66a78d8f
ndequeker <niels@nd-software.be>
2026-02-23 16:16:06 +0100

    Add test for accepting a non-existing invitation

--- organization-api/pkg/organization/invite_accept_test.go
+++ organization-api/pkg/organization/invite_accept_test.go
@@ -7,6 +7,7 @@ import (
 	"connectrpc.com/connect"
 	organizationv1 "github.com/fundament-oss/fundament/organization-api/pkg/proto/gen/v1"
 	"github.com/fundament-oss/fundament/organization-api/pkg/proto/gen/v1/organizationv1connect"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -25,4 +26,32 @@ func Test_AcceptInvitation_Unauthenticated(t *testing.T) {
 	var connectErr *connect.Error
 	require.ErrorAs(t, err, &connectErr)
 	assert.Equal(t, connect.CodeUnauthenticated, connectErr.Code())
+}
+
+func Test_AcceptInvitation_DoesNotExist(t *testing.T) {
+	t.Parallel()
+
+	orgID := uuid.New()
+	userID := uuid.New()
+
+	env := newTestAPI(t,
+		WithOrganization(orgID, "test-org"),
+		WithUser(userID, "test-user", "", nil, []uuid.UUID{orgID}),
+	)
+
+	token := env.createAuthnToken(t, userID)
+
+	client := organizationv1connect.NewInviteServiceClient(env.server.Client(), env.server.URL)
+
+	req := connect.NewRequest(&organizationv1.AcceptInvitationRequest{
+		Id: uuid.New().String(),
+	})
+	req.Header().Set("Authorization", "Bearer "+token)
+	req.Header().Set("Fun-Organization", orgID.String())
+
+	_, err := client.AcceptInvitation(context.Background(), req)
+
+	var connectErr *connect.Error
+	require.ErrorAs(t, err, &connectErr)
+	assert.Equal(t, connect.CodeNotFound, connectErr.Code())
 }
