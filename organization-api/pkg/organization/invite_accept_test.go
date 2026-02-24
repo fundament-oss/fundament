13fa237f9390a7f77e78c4c7041445aa228e4074
ndequeker <niels@nd-software.be>
2026-02-24 09:44:36 +0100

    Add test for accepting an invitation

--- organization-api/pkg/organization/invite_accept_test.go
+++ organization-api/pkg/organization/invite_accept_test.go
@@ -55,3 +55,59 @@ func Test_AcceptInvitation_DoesNotExist(t *testing.T) {
 	require.ErrorAs(t, err, &connectErr)
 	assert.Equal(t, connect.CodeNotFound, connectErr.Code())
 }
+
+func Test_AcceptInvitation_HappyFlow(t *testing.T) {
+	t.Parallel()
+
+	orgID := uuid.New()
+	userID := uuid.New()
+	userToInviteUUID := uuid.New()
+
+	env := newTestAPI(t,
+		WithOrganization(orgID, "test-org"),
+		WithUser(userID, "test-user", "", nil, []uuid.UUID{orgID}),
+		WithUser(userToInviteUUID, "test-user2", "foo@bar.baz", nil, []uuid.UUID{}),
+	)
+
+	token := env.createAuthnToken(t, userID)
+
+	client := organizationv1connect.NewInviteServiceClient(env.server.Client(), env.server.URL)
+
+	req := connect.NewRequest(&organizationv1.InviteMemberRequest{
+		Email:      "foo@bar.baz",
+		Permission: "viewer",
+	})
+	req.Header().Set("Authorization", "Bearer "+token)
+	req.Header().Set("Fun-Organization", orgID.String())
+
+	_, err := client.InviteMember(context.Background(), req)
+	require.NoError(t, err)
+
+	userToInviteToken := env.createAuthnToken(t, userToInviteUUID)
+
+	listReq := connect.NewRequest(&organizationv1.ListInvitationsRequest{})
+	listReq.Header().Set("Authorization", "Bearer "+userToInviteToken)
+	listReq.Header().Set("Fun-Organization", orgID.String())
+
+	invitationsRes, err := client.ListInvitations(context.Background(), listReq)
+	require.NoError(t, err)
+
+	require.NotNil(t, invitationsRes)
+	require.Len(t, invitationsRes.Msg.Invitations, 1)
+
+	acceptInvitationReq := connect.NewRequest(&organizationv1.AcceptInvitationRequest{
+		Id: invitationsRes.Msg.Invitations[0].Id,
+	})
+	acceptInvitationReq.Header().Set("Authorization", "Bearer "+userToInviteToken)
+	acceptInvitationReq.Header().Set("Fun-Organization", orgID.String())
+
+	res, err := client.AcceptInvitation(context.Background(), acceptInvitationReq)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	invitationsRes, err = client.ListInvitations(context.Background(), listReq)
+	require.NoError(t, err)
+
+	require.NotNil(t, invitationsRes)
+	require.Len(t, invitationsRes.Msg.Invitations, 0)
+}
