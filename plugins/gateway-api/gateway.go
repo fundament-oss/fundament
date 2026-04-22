1706794728e8a67add2244571efca062e77ddf9e
Chiel Timmermans <chiel.timmermans@aceworks.nl>
2026-04-22 21:51:42 +0200

    feat: plugin gateway api

---
+++ plugins/gateway-api/gateway.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+const gatewayTemplate = `apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  gatewayClassName: istio
+  listeners:
+    - name: http
+      protocol: HTTP
+      port: 80
+      allowedRoutes:
+        namespaces:
+          from: All
+    - name: https
+      protocol: HTTPS
+      port: 443
+      tls:
+        mode: Terminate
+      allowedRoutes:
+        namespaces:
+          from: All
+`
+
+const gatewayWithCertManagerTemplate = `apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: %s
+  namespace: %s
+  annotations:
+    cert-manager.io/cluster-issuer: letsencrypt
+spec:
+  gatewayClassName: istio
+  listeners:
+    - name: http
+      protocol: HTTP
+      port: 80
+      allowedRoutes:
+        namespaces:
+          from: All
+    - name: https
+      protocol: HTTPS
+      port: 443
+      tls:
+        mode: Terminate
+      allowedRoutes:
+        namespaces:
+          from: All
+`
+
+func buildDefaultGateway(cfg pluginConfig, certManagerAvailable bool) []byte {
+	if certManagerAvailable {
+		return fmt.Appendf(nil, gatewayWithCertManagerTemplate, cfg.GatewayName, cfg.GatewayNamespace)
+	}
+	return fmt.Appendf(nil, gatewayTemplate, cfg.GatewayName, cfg.GatewayNamespace)
+}
